2f56ab41632965132b4c747cc9bd805157a06e4b
unknown <rakeshsmusale03@gmail.com>
2021-11-25 13:42:35 -0800

    Welcome messge

---
+++ IndianSystemCensusAnalyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianSystemCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Indian Census System Problems..!");
+            Console.ReadLine();
+        }
+    }
+}
