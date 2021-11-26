cf3ad95faae06b21a970a5a5717b2c9f1bbd4815
unknown <rakeshsmusale03@gmail.com>
2021-11-26 12:02:03 -0800

    Push all code to main

---
+++ IndianSystemCensusAnalyzer/CSVAdapterFactory.cs
@@ -0,0 +1,28 @@
+﻿using IndianSystemCensusAnalyzer;
+using IndianSystemCensusAnalyzer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianSystemCensusAnalyzer
+{
+    public class CSVAdapterFactory
+    {
+        public static Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.Exception.NO_SUCH_COUNTRY);
+
+            }
+        }
+
+    }
+}
---
+++ IndianSystemCensusAnalyzer/CensusAnalyserException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianSystemCensusAnalyzer
+{
+    
+        public class CensusAnalyserException : Exception
+        {
+            public Exception exception;
+
+            public enum Exception
+            {
+                FILE_NOT_FOUND, INVALID_FILE_TYPE, INCORRECT_HEADER,
+                NO_SUCH_COUNTRY,
+                INCOREECT_DELIMITER
+            }
+
+            public CensusAnalyserException(string message, Exception exception) : base(message)
+            {
+                this.exception = exception;
+            }
+        }
+    
+}
